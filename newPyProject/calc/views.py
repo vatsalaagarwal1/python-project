224dcafc643d18518d97c5e43fff9d1a14ca440f
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2024-01-19 10:55:27 +0530

    added website template
    added static files

--- newPyProject/calc/views.py
+++ newPyProject/calc/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request,'home.html',{'name': 'Vatsala'})
+
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    res = val1 + val2
+
+    return render(request, 'result.html', {'result': res})
