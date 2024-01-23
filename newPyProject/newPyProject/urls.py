7b51693c209d602a06c924cd62a45c139a6e057e
Vatsala Agarwal <agarwalvatsala2002@gmail.com>
2024-01-23 15:40:16 +0530

    made changes

--- newPyProject/newPyProject/urls.py
+++ newPyProject/newPyProject/urls.py
@@ -18,14 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
-
 
 urlpatterns = [
     path('',include('travellor.urls')),
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls'))
 ]
-urlpatterns += staticfiles_urlpatterns() # new
 
 urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
